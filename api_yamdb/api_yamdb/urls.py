9e6ebab2bfe40bdd35fe55270b9b606d5d554a61
proforg77 <ps@parametrix.su>
2022-07-13 15:09:25 +0300

    del top path

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -4,11 +4,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path(
-        '',
-        TemplateView.as_view(template_name='index.html'),
-        name='index'
-    ),
-    path(
         'redoc/',
         TemplateView.as_view(template_name='api/redoc.html'),
         name='redoc'
